daff3d65ae52dba3b93c2f6ac8789420a4222fc7
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-14 20:38:37 +0530

    Add project files.

---
+++ CircleProject/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleProject
+{
+    internal class Circle
+    {
+        double r, a;
+        public Circle()
+        {
+            r = 1.5;
+            
+
+        }
+        public Circle(double r)
+        { this.r = r;
+            
+
+        }
+        public double CalculateArea()
+        { return a = 3.14 * r * r;
+
+        }
+        public double display()
+        { return a;
+        }   
+
+             
+          
+    }
+}
---
+++ CircleProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c=new Circle();
+            Console.WriteLine(c.display());
+            Circle c2=new Circle(1.5);
+            Console.WriteLine(c2.display());
+
+        }
+    }
+}
